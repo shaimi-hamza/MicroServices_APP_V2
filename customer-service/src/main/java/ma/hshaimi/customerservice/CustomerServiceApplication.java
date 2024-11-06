db2d961b6dcd092e4d4766dcdf87ee077bf943b1
shaimi-hamza <shaimihamza@gmail.com>
2024-11-06 13:11:13 +0100

    Initial commit

---
+++ customer-service/src/main/java/ma/hshaimi/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,36 @@
+package ma.hshaimi.customerservice;
+
+import ma.hshaimi.customerservice.entities.Customer;
+import ma.hshaimi.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+		return args -> {
+			customerRepository.save(Customer.builder()
+							.name("hamza").email("hamza@gmail.com")
+					.build());
+			customerRepository.save(Customer.builder()
+					.name("yassmine").email("yassmine@gmail.com")
+					.build());
+			customerRepository.findAll().forEach(c->{
+				System.out.println("------------------");
+				System.out.println(c.getId());
+				System.out.println(c.getName());
+				System.out.println(c.getEmail());
+				System.out.println("------------------");
+			});
+		};
+	}
+
+}
