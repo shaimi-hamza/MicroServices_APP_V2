6634d59a2391b74b8a2d9e092ac1db821991f720
shaimi-hamza <shaimihamza@gmail.com>
2024-12-17 22:32:46 +0100

    config-service

--- customer-service/src/main/java/ma/hshaimi/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/hshaimi/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package ma.hshaimi.customerservice;
 
+import ma.hshaimi.customerservice.config.CustomerConfigParams;
 import ma.hshaimi.customerservice.entities.Customer;
 import ma.hshaimi.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties({CustomerConfigParams.class})
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
