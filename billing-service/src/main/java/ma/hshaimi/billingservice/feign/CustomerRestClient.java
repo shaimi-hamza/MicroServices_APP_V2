9a21ce18de26a56b92b4b90619e538883661dfa3
shaimi-hamza <shaimihamza@gmail.com>
2024-12-29 01:23:15 +0100

    Set up the configuration repository and ensure that properties are accessible from GitHub.

--- billing-service/src/main/java/ma/hshaimi/billingservice/feign/CustomerRestClient.java
+++ billing-service/src/main/java/ma/hshaimi/billingservice/feign/CustomerRestClient.java
@@ -6,10 +6,11 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 @FeignClient(name  = "customer-service")
 public interface CustomerRestClient {
     @GetMapping("/api/customers/{id}")
-    Customer findCustomerById(@PathVariable Long id);
+    Customer getCustomerById(@PathVariable Long id);
 
     @GetMapping("/api/customers")
     PagedModel<Customer> getAllCustomers();
--- billing-service/src/main/java/ma/hshaimi/billingservice/web/BillRestController.java
+++ billing-service/src/main/java/ma/hshaimi/billingservice/web/BillRestController.java
@@ -1,19 +1,35 @@
 package ma.hshaimi.billingservice.web;
 
 
+import ma.hshaimi.billingservice.entities.Bill;
+import ma.hshaimi.billingservice.feign.CustomerRestClient;
 import ma.hshaimi.billingservice.feign.ProductRestClient;
 import ma.hshaimi.billingservice.repository.BillRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
 import ma.hshaimi.billingservice.repository.ProductItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BillRestController {
     @Autowired
     private BillRepository billRepository;
     @Autowired
-    private ProductRestClient ProductItemRepository;
+    private ProductItemRepository productItemRepository;
+    @Autowired
+    private CustomerRestClient customerRestClient;
+    @Autowired
+    private ProductRestClient productRestClient;
 
-
-
+    @GetMapping(path = "/bills/{id}")
+    public Bill getBill(@PathVariable Long id) {
+        Bill bill = billRepository.findById(id).get();
+        bill.setCustomer(customerRestClient.getCustomerById(bill.getCustomerId()));
+        bill.getProductItems().forEach(productItem ->
+        {
+            productItem.setProduct(productRestClient.getProductById(productItem.getProductId()));
+        });
+        return bill;
+    }
 }
